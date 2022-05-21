136cb503a5f5ea0b0b072223929bf8b80939f87d
RomaxDIT <103980742+RomaxDIT@users.noreply.github.com>
2022-05-21 05:44:05 +0500

    Update Main.java

--- GitTest/src/com/company/Main.java
+++ GitTest/src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Register R1 = new Register();
+        R1.reg("Roman", "lionroar439");
+        for (int i = 0; i < R1.registered.size(); i++) {
+            System.out.println(R1.registered.get(i).getName());
+        }
     }
 }
+
