22c5bb086d3c4090dad58d7645146f30ad05fc3e
RomaxDIT <103980742+RomaxDIT@users.noreply.github.com>
2022-05-21 06:02:59 +0500

    Update Register.java

--- GitTest/src/com/company/Register.java
+++ GitTest/src/com/company/Register.java
@@ -1,21 +1,35 @@
 package com.company;
-
 import java.util.ArrayList;
 
-public class Register
-{
-    ArrayList<User> registrated =new ArrayList<>();
+public class Register {
+    ArrayList<User> registered = new ArrayList();
 
-    public void reg(String name,String pass)
-    {
-
+    public Register() {
     }
-    public void reg(String name)
-    {
 
+    public void reg(String name, String pass) {
+        User new_user = new User();
+        new_user.setPassword(pass);
+        new_user.setName(name);
+        registered.add(new_user);
     }
-    public void del(String name,String pass)
-    {
 
+    public void reg (String name){
+
+        String pass = "";
+        String password = "QqWwEeRrTtYyUuIiOoPpAaSsDdFfGgHhJjKkLlZzXxCcVvBbNnMm1234567890";
+        User new_user = new User();
+        new_user.setPassword(pass);
+        new_user.setName(name);
+        registered.add(new_user);
+    }
+
+
+    public void del(String name, String pass)
+    {
+        int del;
+        for (del=0;del<registered.size();del++);
+        if (registered.get(del).GetName().equals(name)&& registered.get(del).GetPassword().equals(pass));
+        {registered.remove(del);}
     }
 }
