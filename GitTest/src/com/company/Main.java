365f6aa3a8ceccb614e1403b98d3081d83156eba
RomaxDIT <103980742+RomaxDIT@users.noreply.github.com>
2022-06-05 22:55:35 +0500

    Update Main.java

--- GitTest/src/com/company/Main.java
+++ GitTest/src/com/company/Main.java
@@ -1,13 +1,16 @@
 package com.company;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        ArrayList<User> registered = new ArrayList();
+
         Register R1 = new Register();
         R1.reg("Roman", "lionroar439");
         for (int i = 0; i < R1.registered.size(); i++) {
-            System.out.println(R1.registered.get(i).getName());
+            System.out.println(R1.registered.get(i).GetName());
         }
     }
 }
-
